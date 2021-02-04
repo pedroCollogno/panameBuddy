cea6e731f8ea048c3e5a0a68c874143cb5ac2cd1
Pierre Collignon <pierrecollignon.dev@gmail.com>
2021-02-04 16:03:48 +0100

    fix: convert axios call to TS

---
+++ src/services/paris-requests.ts
@@ -0,0 +1,9 @@
+import axios, {AxiosResponse} from "axios";
+
+const API_URL = "http://opendata.paris.fr/api/records/1.0/search/?dataset=stationnement-voie-publique-emplacements&q=&facet=regpri&facet=regpar&facet=typsta&facet=arrond&facet=zoneres&facet=tar&facet=locsta&facet=parite&facet=signhor&facet=signvert&facet=confsign&facet=typemob&facet=zoneasp&facet=stv&facet=prefet&facet=mtlast_edit_date_field&facet=datereleve&refine.regpar=V%C3%A9los";
+
+export const getParkingSpots = () => {
+    return axios.get<Object>(API_URL).then((response: AxiosResponse) => {
+        console.log(response.data);
+    });
+}
