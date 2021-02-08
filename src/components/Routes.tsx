a25d09d4439b95a520ba94b51a756a3211cacf56
Pierre Collignon <pierrecollignon.dev@gmail.com>
2021-02-08 18:17:46 +0100

    feat: creat navbar component, add to router

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -2,10 +2,12 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import Favorites from "./Favorites";
 import MapContainer from "./MapContainer";
+import Navbar from "./Navbar";
 
 function Routes() {
     return(
         <Router>
+            <Navbar />
             <Switch>
                 <Route exact path="/favorites" component={Favorites} />
                 <Route path="/" component={MapContainer} />
