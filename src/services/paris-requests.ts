19d4ec161506885c29701882e5f75b5b94e6e7f8
Pierre Collignon <pierrecollignon.dev@gmail.com>
2021-02-10 12:34:24 +0100

    fix: clarify API route

--- src/services/paris-requests.ts
+++ src/services/paris-requests.ts
@@ -1,7 +1,11 @@
 import axios, { AxiosResponse } from "axios";
 
-const API_URL =
-	"http://opendata.paris.fr/api/records/1.0/search/?dataset=stationnement-voie-publique-emplacements&q=&facet=regpri&facet=regpar&facet=typsta&facet=arrond&facet=zoneres&facet=tar&facet=locsta&facet=parite&facet=signhor&facet=signvert&facet=confsign&facet=typemob&facet=zoneasp&facet=stv&facet=prefet&facet=mtlast_edit_date_field&facet=datereleve&refine.regpar=V%C3%A9los";
+const datasetName = "stationnement-voie-publique-emplacements";
+const requiredFieldsQuery =
+	"&facet=regpri&facet=regpar&facet=typsta&facet=arrond&facet=zoneres&facet=tar&facet=locsta&facet=parite&facet=signhor&facet=signvert&facet=confsign&facet=typemob&facet=zoneasp&facet=stv&facet=prefet&facet=mtlast_edit_date_field&facet=datereleve";
+const targetTransport = "V%C3%A9los";
+
+const API_URL = `http://opendata.paris.fr/api/records/1.0/search/?dataset=${datasetName}&q=${requiredFieldsQuery}&refine.regpar=${targetTransport}`;
 
 export const getParkingSpots = () => {
 	return axios.get<Object>(API_URL).then((response: AxiosResponse) => {
