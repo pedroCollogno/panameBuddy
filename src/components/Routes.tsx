74dc839da9b1f8437744b49bd5dcf5dee1f7eab0
Pierre Collignon <pierrecollignon.dev@gmail.com>
2021-02-11 04:07:47 +0100

    fix: routing done right

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,16 +1,28 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from "react-router-dom";
 
 import Favorites from "./Favorites";
 import MapContainer from "./MapContainer";
 import Navbar from "./Navbar";
 
 function Routes() {
+	const [favorites, setFavorites] = useState([]);
 	return (
 		<Router>
 			<Navbar />
 			<Switch>
-				<Route exact path="/favorites" component={Favorites} />
-				<Route path="/" component={MapContainer} />
+				<Route exact path="/favorites">
+					<Favorites />
+				</Route>
+				<Route exact path="/map" component={MapContainer} />
+				<Route path="/">
+					<Redirect to="/map" />
+				</Route>
 			</Switch>
 		</Router>
 	);
